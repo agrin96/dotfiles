1ce16d2e0e2d65a03e0c3a119bc43391bebcd135
Aleksandr Grin <Grinsasha@hotmail.com>
2024-11-12 20:22:46 -0500

    Changed minimap to be off by default. Snipe added options

--- nvim/lua/plugins/mini-map.lua
+++ nvim/lua/plugins/mini-map.lua
@@ -6,11 +6,15 @@ return {
     opts = {},
     keys = {
         -- Buffer-Specific Minimap Controls
-        { "<leader>nm", "<cmd>Neominimap bufToggle<cr>", desc = "Toggle minimap for current buffer" },
-        { "<leader>nr", "<cmd>Neominimap bufRefresh<cr>", desc = "Refresh minimap for current buffer" },
+        { "<leader>mm", "<cmd>Neominimap bufToggle<cr>", desc = "Toggle minimap for current buffer" },
+        { "<leader>mr", "<cmd>Neominimap bufRefresh<cr>", desc = "Refresh minimap for current buffer" },
+        { "<leader>mt", "<cmd>Neominimap toggle<cr>", desc = "Toggle global minimap" },
     },
     init = function()
         vim.g.neominimap = {
+			-- Do not enable by default because we just don't need it on a lot of files.
+			-- Plus it cuases errors when opening blanks
+			auto_enable = false,
             x_multiplier = 2,
             float = {
                 window_border = "none",
