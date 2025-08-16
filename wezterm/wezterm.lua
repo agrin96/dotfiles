e244f09b911201297e06c4081459deaedbf914ec
Aleksandr Grin <Grinsasha@hotmail.com>
2025-08-15 23:46:19 -0400

    fixed terminal padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -27,6 +27,7 @@ config.show_new_tab_button_in_tab_bar = false
 config.adjust_window_size_when_changing_font_size = false
 config.scrollback_lines = 5000
 config.window_background_opacity = 0.98
+
 local default_padding = {
     left = "1.5cell",
     right = "1.5cell",
@@ -35,23 +36,17 @@ local default_padding = {
 }
 config.window_padding = default_padding
 
--- Allows us to listen to user variable changes to customize config.
 -- In this case we will override the default window padding to 0 when
 -- we enter nvim because we want to get rid of weird borders
-wezterm.on("user-var-changed", function (window, pane, name, value)
-    if name == "NVIM_ENTER" then
-        local overrides = window:get_config_overrides() or {}
-        if value == "1" then
-            overrides.window_padding = {
-                left = 0,
-                right = 0,
-                top = 0,
-                bottom = 0,
-            }
-        else
-            overrides.window_padding = default_padding
-        end
-        window:set_config_overrides(overrides)
+wezterm.on("update-status", function(window, pane)
+    if pane:is_alt_screen_active() then
+        window:set_config_overrides({
+            window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+        })
+    else
+        window:set_config_overrides({
+            window_padding = default_padding,
+        })
     end
 end)
 
@@ -61,7 +56,7 @@ config.window_decorations = "RESIZE|MACOS_FORCE_SQUARE_CORNERS"
 
 local border_color = '#37364a'
 config.window_frame = {
-    font = wezterm.font { family = "Maple Mono NF", weight = "Medium"},
+    font = wezterm.font { family = "Maple Mono NF", weight = "Medium" },
     font_size = 15,
     active_titlebar_bg = "none",
     inactive_titlebar_bg = "none",
