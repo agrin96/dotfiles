f99817eeabd9a0c1174ae20c6128c214dda4f72c
Aleksandr Grin <Grinsasha@hotmail.com>
2025-08-15 21:03:37 -0400

    gitconflict highlights

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -5,16 +5,16 @@ return {
     keys = {
         {'<leader>go', '<Plug>(git-conflict-ours)', desc = "Choose ours (current)", mode = {'n'} },
         {'<leader>gt', '<Plug>(git-conflict-theirs)', desc = "Choose theirs (incoming)", mode = {'n'} },
-        {'<leader>g0', '<Plug>(git-conflict-both)', desc = "Choose none of the changes", mode = {'n'} },
-        {'<leader>gb', '<Plug>(git-conflict-none)', desc = "Choose both of the changes", mode = {'n'} },
+        {'<leader>gb', '<Plug>(git-conflict-both)', desc = "Choose both of the changes", mode = {'n'} },
+        {'<leader>g0', '<Plug>(git-conflict-none)', desc = "Choose none of the changes", mode = {'n'} },
         {'g]', '<Plug>(git-conflict-next-conflict)', desc = "Go to next git conflict", mode = {'n'} },
         {'g[', '<Plug>(git-conflict-previous-conflict)', desc = "Go to previous git conflict", mode = {'n'} },
     },
     config = function ()
-        -- vim.api.nvim_set_hl(0, 'DiffAdd', {
-        --     fg = '',
-        --     bg = '',
-        -- })
+        vim.api.nvim_set_hl(0, 'DiffText', {
+            fg = '#1c2025',
+            bg = '#324c69',
+        })
         require('git-conflict').setup({
             highlights = {
                 incoming = 'DiffAdd',
