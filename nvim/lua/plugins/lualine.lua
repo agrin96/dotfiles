f6477c2867f8a439d96dc4331a7ec0a83af93827
Aleksandr Grin <Grinsasha@hotmail.com>
2025-08-20 00:30:16 -0400

    lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,25 +6,36 @@ return {
     },
     config = function()
         local lualine = require("lualine")
+        local text_color = '#dcdeef'
+        local section_z_background = '#2c323a'
+
         lualine.setup({
             options = {
                 theme = "onedark",
                 globalstatus = true,
                 section_separators = {
-                    -- left = "",
-                    -- right = "",
                     left = "█",
                     right = "█",
                 },
+                component_separators = {
+                    left = "█",
+                    right = "█",
+                }
             },
             sections = {
                 lualine_a = {"mode"},
                 lualine_b = {"searchcount"},
                 lualine_c = {},
                 lualine_x = {"diff","diagnostics"},
-                lualine_y = {"location"},
-                lualine_z = {"lsp_status", "filetype"},
+                lualine_y = {
+                    { "location", color = { fg = text_color } }
+                },
+                lualine_z = {
+                    { "lsp_status", color = { fg = text_color, bg = section_z_background} },
+                    { "filetype", color = {fg = text_color, bg = section_z_background} },
+                },
             }
         })
     end
+
 }
